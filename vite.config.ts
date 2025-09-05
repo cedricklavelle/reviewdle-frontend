e11aa9662969e016eadb3c8fa436189a957d5af2
CedL1999 <cedlav@gmail.com>
2025-09-04 21:37:47 -0400

    pushed more stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    react(), 
+    svgr({
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+    }),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
   ],
+  optimizeDeps: {
+    include: [
+      "@mui/material",
+      "@mui/icons-material",
+      "@mui/lab"
+    ]
+  }
 })
